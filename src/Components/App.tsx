d291cf9f8ac3306df02a0d222d8d8336f20dede8
Krzysztof Czaplejewicz <czaplejewicz.krzysztof@gmail.com>
2018-10-21 16:43:36 +0200

    init

---
+++ src/Components/App.tsx
@@ -0,0 +1,20 @@
+import * as React from "react";
+
+class App extends React.Component<any, any> {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Hello Little boy</h1>
+        <h1>Hello Little boy </h1>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/index.tsx
@@ -0,0 +1,7 @@
+import * as React from "react";
+import App from "./Components/App";
+import * as ReactDOM from "react-dom";
+import "./styles/app.scss"
+const ROOT = document.getElementById("root");
+ReactDOM.render(<App />, ROOT);
+
